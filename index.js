98ea213751612c7ee8b5b1399ad287a19f98c5b5
Lana Worobec <llama.p0rtk3y@gmail.com>
2020-02-02 18:19:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ function wagesEarnedOnDate(employee, date){
 function allWagesFor(employee){
   let dates = employee.timeInEvents.map(em => em.date)
   let wageOnDate = dates.map(date => wagesEarnedOnDate(employee, date))
-  return wageOnDate;
+  return wageOnDate.reduce(sum, date)
 }
