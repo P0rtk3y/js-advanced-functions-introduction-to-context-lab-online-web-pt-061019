2e66d3a84f0f43f22f260c047f7cdd664ac91d51
Lana Worobec <llama.p0rtk3y@gmail.com>
2020-02-02 18:04:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+function createEmployeeRecord(employee) {
+    // returns JavaScript Object with keys:
+    return {
+        firstName: employee[0],  
+        familyName:employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeArray){
+  return employeeArray.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(employee, timeInStamp){
+  let [date, time] = tmeInStamp.split(' ')
+  employee.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(time, 10),
+    date: date
+  })
+  return employee;
+}
